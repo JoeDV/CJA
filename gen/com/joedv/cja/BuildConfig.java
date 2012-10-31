6b71a0809357bc63716ca9c50d4823c01f4e3416
Joe Del Vecchio <Josephdelvecchio@gmail.com>
2012-10-31 09:59:15 -0400

    First commit

---
+++ gen/com/joedv/cja/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.joedv.cja;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/joedv/cja/CJAActivity.java
@@ -0,0 +1,26 @@
+package com.joedv.cja;
+
+import com.joedv.cja.R;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import org.apache.cordova.*;
+
+@SuppressWarnings("unused")
+public class CJAActivity extends DroidGap {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+				 WindowManager.LayoutParams.FLAG_FULLSCREEN | 
+				 WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+		
+		super.setIntegerProperty("splashscreen", R.drawable.splash);
+		super.loadUrl("file:///android_asset/www/index.html",3000);
+	}
+}
